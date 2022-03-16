efcba35022f7c19f84f0374d9393911f71faa18d
abxxbo <devavcado@gmail.com>
2022-03-15 21:38:32 -0500

    Refactor lots of code

--- titlebars.sh
+++ titlebars.sh
@@ -1,42 +1,75 @@
 #!/bin/bash
 
+#  __________________
+# |__________________|
+# |                  |
+# |                  |
+# |                  |
+# |                  |
+# |__________________|
+#
+# Titlebars for bspwm.
 
-# run lemonbar script
+
+# Run our lemonbar script
+
+function run_lemonbar(){
+  if [[ "$#" == "0" ]]; then
+    bash ~/.config/bar/bar.sh | lemonbar -B "#000" -d |
+      while read line; do
+        IFS=$'\t' read -ra cmd <<< "${line}"
+        case "${cmd[0]}" in
+          exit) bspc node -c ;; # Quit current app
+          maxi) bspc node -t fullscreen ;; # Maximize to full screen
+          *) ;; # Don't do anything
+        esac
+      done & # Run in background
+  else
+    bash ~/.config/bar/bar.sh | lemonbar -B $1 -d |
+      while read line; do
+        IFS=$'\t' read -ra cmd <<< "${line}"
+        case "${cmd[0]}" in
+          exit) bspc node -c ;;
+          maxi) bspc node -t fullscreen ;;
+          *) ;;
+        esac
+      done &
+  fi
+}
+
+## Can be switched from top to one of these values
+# top    (already here -- default),
+# left   (left side of window),
+# right  (right side of window),
+# bottom (bottom side of window)
+state="top"
 
 if [[ "$#" == "0" ]]; then
-  bash ~/.config/bar/bar.sh | lemonbar -B "#000" -d |
-    while read line; do
-      IFS=$'\t' read -ra cmd <<< "${line}"
-      case "${cmd[0]}" in
-        exit) bspc node -c ;;
-        maxi) bspc node -t fullscreen ;;
-        *) ;;
-      esac
-    done &
+  run_lemonbar
 else
-  # 1st argument
-  case $1 in
-    "--help")
-      # cat << EOF dont work :(
-      echo "Usage: titlebars.sh [Background Color] [Position -- left, right, top, or bottom]"
-      exit
-      ;;
-    *)
-      ;;
-  esac
-  # 2st argument
   case $2 in
     "left")
       ;;
     "right")
       ;;
     "top")
+      echo "Already set to this."
       ;;
     "bottom")
       ;;
     *)
-      echo "what?"
+      echo "unrecognized command, please choose from one of the following: top, bottom, left, right"
+      echo "Default setting is 'TOP'"
       exit
+      ;;
+  esac
+  case $1 in
+    "--help")
+      echo "Usage: titlebars.sh [BG color] [Position on window: left, right, top (default) or bottom]"
+      exit
+      ;;
+    *)
+      run_lemonbar "$1"
       ;;
   esac
 fi
